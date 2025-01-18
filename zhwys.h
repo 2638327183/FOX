da7e238950217c6d46e6927274d498af5812c31b
jiaohuadehuli <129431801+2638327183@users.noreply.github.com>
2025-01-18 17:00:19 +0800

    Add files via upload

--- zhwys.h
+++ zhwys.h
@@ -4,12 +4,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-#define q "\033[31;6m"
-#define w "\033[31;0m"
+#include <time.h>
+#define w "\033[31;6m" //红色
+#define q "\033[31;0m" //结尾
 
 int 总量,小时,多少数,乘,exti;
 int gly=0;
 int syz=1314520;
+int z,zh,zhw;
 
 #define 蓝色 SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),0x01)
 #define 绿色 SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),0x02)
